2f584d6d683d00d6e878601658f136f4af97ffb2
guzzye13 <eddyguzman322@gmail.com>
2022-07-20 18:45:30 -0400

    Variables demo

---
+++ variables.py
@@ -0,0 +1,10 @@
+# Explain simple easy variables
+students_count = 1000 # int
+rating = 4.99 # float
+is_published = False # bool
+course_name = "Python programming" # str
+
+print(type(students_count)) # type() displays the type of variable of a given object/data element
+print(type(rating))
+print(type(is_published))
+print(type(course_name))
